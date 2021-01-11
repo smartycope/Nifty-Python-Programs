01ec8c6b9f04f1b3493592d0b78c7e32064ca9ed
Copeland Carter <60020173+smartycope@users.noreply.github.com>
2021-01-10 18:22:41 -0700

    Added capo functionality

--- noteToTab.py
+++ noteToTab.py
@@ -1,8 +1,14 @@
-inNotes = input('Enter space-seperated notes in the form "G2 C3# A1b":\n')
+inNotes = input('Enter space-seperated notes in the form "G2 C3# A1b". To specify the capo fret, put the fret number at the beginning.:\n')
 
 notes = inNotes.split(' ')
 
 songLen = (len(notes) * 2) + 5
+
+if len(notes[0]) == 1:
+    capo = int(notes[0])
+    notes.pop(0)
+else:
+    capo = 0
 
 
 strings = {
@@ -40,15 +46,17 @@ def getFretNum(Note:str):
 
     assert(octave >= 2)
     if note not in validNotes:
-        raise UserWarning(f"{Note} is not a valid note!")
+        print(f"{Note} is not a valid note!")
+        exit(1)
 
     fretNum += (octave - 2) * 12
     fretNum += noteOrder[note]
     # 4 because the guitar starts at E, not C
     fretNum -= 4
 
-    if not isBetween(fretNum, -1, 47):
-        raise UserWarning(f"You can't play {Note} on the guitar!")
+    if not isBetween(fretNum, capo, 47):
+        print(f"You can't play {Note} on the guitar with a capo on fret {capo}!")
+        exit(1)
 
     return fretNum
 
@@ -76,18 +84,18 @@ def insertChar(string, index, char):
 
 for pos, note in enumerate(notes):
     fret = getFretNum(note)
-    if isBetween(fret, 0, 4):
-        strings['E'] = insertChar(strings['E'], pos * 2, str(fret))
-    elif isBetween(fret, 5, 9):
-        strings['A'] = insertChar(strings['A'], pos * 2, str(fret - 5))
-    elif isBetween(fret, 10, 14):
-        strings['D'] = insertChar(strings['D'], pos * 2, str(fret - 10))
-    elif isBetween(fret, 15, 18):
-        strings['G'] = insertChar(strings['G'], pos * 2, str(fret - 15))
-    elif isBetween(fret, 19, 23):
-        strings['B'] = insertChar(strings['B'], pos * 2, str(fret - 19))
+    if   isBetween(fret, 0+capo, 4+capo):
+        strings['E'] = insertChar(strings['E'], pos * 2, str(fret - capo))
+    elif isBetween(fret, 5+capo, 9+capo):
+        strings['A'] = insertChar(strings['A'], pos * 2, str(fret - 5 - capo))
+    elif isBetween(fret, 10+capo, 14+capo):
+        strings['D'] = insertChar(strings['D'], pos * 2, str(fret - 10 - capo))
+    elif isBetween(fret, 15+capo, 18+capo):
+        strings['G'] = insertChar(strings['G'], pos * 2, str(fret - 15 - capo))
+    elif isBetween(fret, 19+capo, 23+capo):
+        strings['B'] = insertChar(strings['B'], pos * 2, str(fret - 19 - capo))
     else:
-        strings['e'] = insertChar(strings['e'], pos * 2, str(fret - 24))
+        strings['e'] = insertChar(strings['e'], pos * 2, str(fret - 24 - capo))
 
 
 printStrings()
